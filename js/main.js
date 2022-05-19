f42aef67758d06ee5840730d4a8fe361ebeec1f0
Danil <rumyancev_danil@mail.ru>
2022-05-19 13:44:32 +0600

    upadates

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
 function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-    };
-  }
-  
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.day');
-    var hoursSpan = clock.querySelector('.hour');
-    var minutesSpan = clock.querySelector('.minute');
-  
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-  
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
+  let t = Date.parse(endtime) - Date.parse(new Date());
+  let seconds = Math.floor((t / 1000) % 60);
+  let minutes = Math.floor((t / 1000 / 60) % 60);
+  let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+ 
+function initializeClock(id, endtime) {
+  let clock = document.getElementById(id);
+  let hoursSpan = clock.querySelector('.hours');
+  let minutesSpan = clock.querySelector('.minutes');
+  let secondsSpan = clock.querySelector('.seconds');
+ 
+  function updateClock() {
+    let t = getTimeRemaining(endtime);
+ 
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+ 
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
     }
-  
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
   }
+ 
+  updateClock();
+  let timeinterval = setInterval(updateClock, 1000);
+}
+ 
+let deadline = '2022-05-22'
+initializeClock('timer', deadline);
+
+// SLIDER
   
+  let swiper = new Swiper(".mySwiper2", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
